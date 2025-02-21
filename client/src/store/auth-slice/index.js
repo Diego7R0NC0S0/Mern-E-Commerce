af5fde8dbb658faefadceb0782b71febf44d7040
Diego7R0NC0S0 <diego.troncoso.lopez@gmail.com>
2025-02-21 07:47:01 -0300

    21/02/2025

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -1,4 +1,6 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   isAuthenticated: false,
@@ -6,12 +8,37 @@ const initialState = {
   user: null,
 };
 
+export const registerUser = createAsyncThunk(
+  "/auth/register",
+
+  async (formData) => {
+    const response = await axios.post(
+      "http://localhost:5000/api/auth/register",
+      formData,
+      {
+        withCredentials: true,
+      }
+    );
+
+    return response.data;
+  }
+);
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
     setUser: (state, action) => {},
   },
+  extraReducers: (builder)=> {
+    builder.addCase(registerUser.pending, (state)=> {
+      state.isLoading = true
+    }).addCase(registerUser.fulfilled, (state, action)=> {
+      state.isLoading = false;
+      state.user = action.payload;
+      state.isAuthenticated = false
+    })
+  }
 });
 
 export const { setUser } = authSlice.actions;
